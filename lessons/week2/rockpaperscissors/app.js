33afefb5614211608bbf0bf68e0eb232ceeb036d
Luis <lalo_luis_6@hotmail.com>
2021-06-05 14:06:19 -0500

    Todo el proyecto terminado

--- lessons/week2/rockpaperscissors/app.js
+++ lessons/week2/rockpaperscissors/app.js
@@ -1,3 +1,6 @@
+var userScore = 0
+var computerScore = 0
+
 const Rock = document.getElementById("r")
 const Paper = document.getElementById("p")
 const Scissors = document.getElementById("s")
@@ -7,16 +10,74 @@ const computerScore_p = document.getElementById('computer-score')
 
 const message = document.getElementById("message")
 
-function game(){
-    console.log('Hola')
+function getComputerChoice(){
+    const Choices = ['r', 'p', 's']
+    let random = Math.floor(Math.random()*3 )
+    return Choices[random]
+}
+
+function updateScore(){
+    userScore_p.innerHTML = userScore
+    computerScore_p.innerHTML = computerScore
+}
+
+function convert(choice){
+    if (choice === 'r' ) return 'Piedra'
+    if (choice === 'p' ) return 'Papel'
+    return 'Tijeras'
+}
+
+function lose(user, computer){
+    computerScore++
+    // updateScore()
+    message.innerHTML = `${convert(user)} pierde contra ${convert(computer)}, entonces perdiste 😥`
+}
+function win(user, computer){
+    userScore++
+    // updateScore()
+    message.innerHTML = `${convert(user)} le gana a ${convert(computer)}, entonces ganaste 😎`
+}
+function draw(user, computer){
+    message.innerHTML = `${convert(user)} es igual a ${convert(computer)}, hay empate 😑`
+}
+
+function changeColors(HTMLElement, state){
+    HTMLElement.className = 'choice ' + state
+    setTimeout( () => {
+        HTMLElement.className = 'choice'
+    }, 1000)
+}
+
+function game(userChoice, Element){
+    let computerChoice = getComputerChoice()
+    switch(userChoice + computerChoice){ // ps
+        case 'ps':
+        case 'sr':
+        case 'rp':
+            changeColors(Element, 'lose')
+            lose(userChoice,computerChoice)
+            break;
+        case 'sp':
+        case 'rs':
+        case 'pr':
+            changeColors(Element, 'win')
+            win(userChoice,computerChoice)
+            break;
+        case 'ss':
+        case 'rr':
+        case 'pp':
+            changeColors(Element, 'draw')
+            draw(userChoice,computerChoice)
+            break;
+    }
 }
 
 Rock.addEventListener('click', function(){
-    game('r')
+    game('r', this)
 })
 Paper.addEventListener('click', function(){
-    game('p')
+    game('p', this)
 })
 Scissors.addEventListener('click', function(){
-    game('s')
+    game('s', this)
 })

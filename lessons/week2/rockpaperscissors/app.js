72f250e9d390aec9609403f46d11f3f205601d68
Luis <lalo_luis_6@hotmail.com>
2021-06-05 13:54:36 -0500

    Obtener elementos HTLM de la web

---
+++ lessons/week2/rockpaperscissors/app.js
@@ -0,0 +1,8 @@
+const Rock = document.getElementById("r")
+const Paper = document.getElementById("p")
+const Scissors = document.getElementById("s")
+
+const userScore_p = document.getElementById('user-score')
+const computerScore_p = document.getElementById('computer-score')
+
+const message = document.getElementById("message")

f81d886f21fed9025d02e572dc8562b2cf46d122
Luis <lalo_luis_6@hotmail.com>
2021-06-05 13:55:45 -0500

    addEventListeners y función Game

--- lessons/week2/rockpaperscissors/app.js
+++ lessons/week2/rockpaperscissors/app.js
@@ -6,3 +6,17 @@ const userScore_p = document.getElementById('user-score')
 const computerScore_p = document.getElementById('computer-score')
 
 const message = document.getElementById("message")
+
+function game(){
+    console.log('Hola')
+}
+
+Rock.addEventListener('click', function(){
+    game('r')
+})
+Paper.addEventListener('click', function(){
+    game('p')
+})
+Scissors.addEventListener('click', function(){
+    game('s')
+})
